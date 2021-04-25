4457f64fca08bcac79e6a54e5515343a9fb7287a
Đạt. Nguyễn Dương Minh Tâm <nguyentamdat@gmail.com>
2021-04-25 23:58:45 +0700

    fix mirgrate

--- app/features/conversation_manager.js
+++ app/features/conversation_manager.js
@@ -176,7 +176,7 @@ module.exports = function (controller) {
 
       await request(options).then(async (response) => {
         debug("Rasa response", response);
-        let isFashion = response.body.action;
+        let isFashion = response.action;
         if (isFashion > _THREAD_HOLD) {
           await bot.reply(message, {
             text: "Bạn có muốn nhận hỗ trợ về sản phẩm này không?",
