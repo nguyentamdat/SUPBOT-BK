8c1b7d4970de021a3aeb88b86cf961c02e33bbf5
Đạt. Nguyễn Dương Minh Tâm <nguyentamdat@gmail.com>
2021-06-01 14:35:39 +0700

    update client

---
+++ admin/index.js
@@ -0,0 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:NhIUyo7mfafwErME@chatbot.y0fph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(async (err) => {
+    const collection = client.db("BotFramework").collection("BotFrameworkState");
+    let cursor = collection.find({});
+    await cursor.forEach(item => {
+        console.log(item);
+    });
+    // perform actions on the collection object
+    client.close();
+});
